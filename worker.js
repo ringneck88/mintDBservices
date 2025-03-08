75a6149687febe19f29c3f693fd52ae77c8f1937
kelly-brightroot <ksharp@brightroot.com>
2025-03-08 12:33:28 -0700

    feat: add Prettier and EditorConfig configuration; update package.json with PostgreSQL dependencies; enhance worker.js for database interactions

--- worker.js
+++ worker.js
@@ -1,31 +1,66 @@
-import { Client } from 'pg-promise';
+import { env } from 'process';
+import postgres from 'postgres';
 
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event));
-});
+export async function onRequest(context) {
+  console.log(`[LOGGING FROM /hello]: Request came from ${context.request.url}`);
 
-async function handleRequest(event) {
-  const { request } = event;
+  return new Response("Hello, world!");
+}
 
-  if (request.method === 'POST') {
-    try {
-      const formData = await request.formData();
-      const name = formData.get('name');
-      const email = formData.get('email');
 
-      const client = new Client({
-        connectionString: DATABASE_URL,
-      });
 
-      await client.connect();
-      await client.query('INSERT INTO users(name, email) VALUES($1, $2)', [name, email]);
-      await client.end();
+export default {
+	async fetch(request, env, ctx) {
+		console.log('DB_USERNAME:', env.DB_USERNAME);
+		console.log('DB_PASSWORD:', env.DB_PASSWORD);
+		console.log('DB_HOST:', env.DB_HOST);
+		console.log('DB_PORT:', env.DB_PORT);
+		console.log('DB_NAME:', env.DB_NAME);
 
-      return new Response('Data saved successfully', { status: 200 });
-    } catch (error) {
-      return new Response('Error saving data', { status: 500 });
-    }
-  }
+		const sql = postgres({
+			username: env.DB_USERNAME,
+			password: env.DB_PASSWORD,
+			host: env.DB_HOST,
+			port: env.DB_PORT,
+			database: env.DB_NAME,
+		});
+console.log('Database connection established', sql);
 
-  return new Response('Method not allowed', { status: 405 });
+		console.log(`Request received: ${request.method} ${request.url}`);
+		const headers = {};
+		for (const [key, value] of request.headers.entries()) {
+			headers[key] = value;
+		}
+		//console.log('Headers:', JSON.stringify(headers, null, 2));
+
+		if (request.method === 'POST') {
+
+			try {
+				//const body = await request.text();
+				const formData = await request.formData();
+				const name = formData.get('name');
+				const email = formData.get('email');
+				console.log('users: ', email, name);
+
+				const users = await sql`
+					insert into users
+					name,
+					email
+					(${ name }, ${ email })
+					returning name, email
+				`;
+				console.log('users: ', users);
+
+				return new Response(users, {
+					headers: { 'content-type': 'text/plain' },
+					status: 200,
+					statusText: 'OK'
+				});
+			} catch (error) {
+				return new Response('Error saving all the data', { status: 500 });
+			}
+		}
+
+		return new Response('Method not allowed', { status: 405 });
+	}
 }
